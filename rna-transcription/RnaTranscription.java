122d71b491b4506ec1d47ec1f4efc2bb331863c9
tanvikansal <37088169+tanvikansal@users.noreply.github.com>
2019-07-17 22:54:24 +0530

    Add files via upload

--- rna-transcription/RnaTranscription.java
+++ rna-transcription/RnaTranscription.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
@@ -8,16 +11,21 @@ class RnaTranscription {
         for(int i=0;i<dnaLength;i++)
         {
             dnaUnit=dnaStrand.charAt(i);
-            if(dnaUnit == 'G')
-                rnaUnit='C';
-            else if(dnaUnit == 'C')
-                rnaUnit = 'G';
-            else if(dnaUnit == 'T')
-                rnaUnit = 'A';
-            else if(dnaUnit == 'A')
-                rnaUnit = 'U';
+            rnaUnit=getRnaUnit(dnaUnit);
             rnaStrand = rnaStrand+Character.toString(rnaUnit);
         }
         return rnaStrand;
+    }
+    public char getRnaUnit(char dnaUnit)
+    {
+        HashMap<Character,Character> dnaRnaMapping =new HashMap<Character,Character>();
+        dnaRnaMapping.put('G', 'C');
+        dnaRnaMapping.put('g', 'C');
+        dnaRnaMapping.put('C', 'G');
+        dnaRnaMapping.put('T','A');
+        dnaRnaMapping.put('t','A');
+        dnaRnaMapping.put('A', 'U');
+        dnaRnaMapping.put('a', 'U');
+        return dnaRnaMapping.get(dnaUnit);
     }
 }
